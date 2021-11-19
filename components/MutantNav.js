16ca01603f568d7a9bdfa9135e60e700e642583a
ethdumplingz <dumplingz.eth@gmail.com>
2021-11-19 16:46:58 -0500

    Deploy Next.js to gh-pages

---
+++ components/MutantNav.js
@@ -0,0 +1,30 @@
+import {Navbar, Container, Col, Row} from "react-bootstrap";
+import Link from "next/link";
+
+const Nav = () => {
+	return (
+		<Navbar bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="#home">
+					<img
+						alt=""
+						src="/logo.svg"
+						width="30"
+						height="30"
+						className="d-inline-block align-top"
+					/>{' '}
+					Mutant Devs
+				</Navbar.Brand>
+				<Row>
+					<Col>
+						<Link href="/about" as={process.env.BACKEND_URL + '/about'}>
+							<a>About</a>
+						</Link>
+					</Col>
+				</Row>
+			</Container>
+		</Navbar>
+	)
+}
+
+export default Nav;
--- out/_next/static/XLn1R6uTRTwkMZfndT_ng/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-359858df1d0f80eb.js"],"/_error":["static/chunks/pages/_error-d828d26f1f5d2954.js"],"/about":["static/chunks/pages/about-8f709c7eaee3a845.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
