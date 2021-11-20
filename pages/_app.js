53f52f24fee3970cbd610cca89b6e7363b3d4411
ethdumplingz <dumplingz.eth@gmail.com>
2021-11-20 00:50:24 -0500

    changed google font include and refactored custom app in nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
+import Head from "next/head";
 export default function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Fishathon</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
