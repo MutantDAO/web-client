d4aba2b1ea3685db77e5857ae5d14c6fe1b958d5
Kathodler <>
2021-11-21 09:14:45 +1000

    Set basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-// const debug = process.env.NODE_ENV !== "production";
+const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {};
+module.exports = {
+  basePath: !debug ? "/web-client/" : "",
+};
